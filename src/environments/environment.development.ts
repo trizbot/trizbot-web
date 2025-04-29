fdc1bb8c9702e3a11b64e4342401e80df87d97be
virct <vtglob25@gmail.com>
2025-04-29 23:16:47 +0100

    added material ui and booostrap

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    // apiBaseUrl: 'http://localhost:3000',
-    apiBaseUrl: 'https://trizbot-api.onrender.com',
+    apiBaseUrl: 'http://localhost:3000',
+    // apiBaseUrl: 'https://trizbot-api.onrender.com',
     companyName:"TrizBot",
     cloudinaryPreset:"trizbot",
     cloudinaryName:"dvkf7xs6c",
@@ -11,3 +11,7 @@ export const environment = {
     cloudUploadApiUrl:"https://api.cloudinary.com/v1_1",
     cloudUrl:"cloudinary://831632495684182:n5wNHD0R88iUKT8QgSlLD02Yj-s@dvkf7xs6c"
   };
+
+
+
+  
--- src/home/welcome/welcome.component.ts
+++ src/home/welcome/welcome.component.ts
@@ -5,17 +5,14 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 
-
 @Component({
   selector: 'app-welcome',
   standalone: true,
   imports: [
     CommonModule,
     RouterModule,
-  
     MatExpansionModule,
-    MatIconModule,
-  
+    MatIconModule,  
   ],
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
