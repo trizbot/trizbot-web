8500eae3eb273ab8950ff3e5167c232912f82d63
omonfesty <excelglobaltech@gmail.com>
2025-09-13 00:02:04 +0100

    update updated

--- src/app/pages/myaccount/users-feature/user-disabled-feature.component.ts
+++ src/app/pages/myaccount/users-feature/user-disabled-feature.component.ts
@@ -8,10 +8,10 @@ import { DisabledService } from './user-disabled-feature.services';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/module.d-DBDMCw5I';
-import { MatFormFieldModule } from '@angular/material/module.d-vndDeG-q';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-user-feature',
